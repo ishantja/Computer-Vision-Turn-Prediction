326a8c4df78990d85b6dc2203d25cb863a84f5cf
ishantja <ishantamrakar24@gmail.com>
2023-11-29 12:32:58 -0500

    all class methods working

--- code/turn_prediction.py
+++ code/turn_prediction.py
@@ -3,6 +3,113 @@
 import cv2
 import numpy as np
 
+# visualizer class is capable of productin 1-5 output windows scaled proportionally to the input sample image
+# it also has the capability to save the generated frames asa a video if used inside a loop
+class Visualizer():
+    def __init__(self, image, scale, frames=[None, None, None, None, None, 'Final Frame'], save=False):
+        self.frame1 = None
+        self.frame2 = None
+        self.frame3 = None
+        self.frame4 = None
+        self.frame0 = None
+
+        self.save = save
+        self.frame_dict = {frames[0]: self.frame1, frames[1]: self.frame2,
+                           frames[2]: self.frame3, frames[3]: self.frame4, frames[4]: self.frame0}
+        self.scale = scale
+
+        self.image = self.resize(image, self.scale)
+        self.frame_height = self.image.shape[0]
+        self.frame_width = self.image.shape[1]
+
+        self.number_of_frames = 1+(4-frames.count(None))
+        self.number_of_small_frames = self.number_of_frames - 1
+        self.aspect_ratio = self.frame_width/self.frame_height
+
+        self.small_frame_width = int(
+            self.frame_width/self.number_of_small_frames)
+        self.small_frame_height = int(
+            (self.number_of_small_frames * self.small_frame_width)/self.aspect_ratio)
+
+        self.capture_width = self.frame_width * self.number_of_small_frames
+        self.capture_height = self.frame_height * \
+            self.number_of_small_frames + self.frame_height
+
+        self.font = cv2.FONT_HERSHEY_SIMPLEX
+        self.fontScale = 1
+        self.color = (255, 255, 255)
+        self.thickness = 2
+
+        self.fps = 25
+        if self.save:
+            save_path = Visualizer.get_absolute_path(
+                "output") + "/lane_detection_output.mp4"
+            self.result = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(
+                *'mp4v'), self.fps, (self.capture_width, self.capture_height))
+
+    @staticmethod
+    def show_image(image):
+        cv2.imshow("preview", image)
+
+
+    @staticmethod
+    def get_absolute_path(dir):
+        current_directory = os.getcwd()
+        parent_directory = os.path.dirname(current_directory)
+        absolute_path = os.path.join(parent_directory, dir)
+        return absolute_path
+
+    def resize(self, image, scale):
+        new_width = int(image.shape[1] * scale)
+        new_height = int(image.shape[0] * scale)
+        image = cv2.resize(image, (new_width, new_height))
+        return image
+
+    def update_frame(self, image, frame_name):
+        if frame_name in self.frame_dict.keys():
+            self.frame_dict[frame_name] = image
+            self.frame_dict[frame_name] = cv2.cvtColor(
+                self.frame_dict[frame_name], cv2.COLOR_BGR2RGB)
+            # tune these two values for text alignment
+            vertical_placement = 0.95
+            horizontal_placement = 0.8
+            org2 = (int(self.frame_width-vertical_placement*self.frame_width),
+                    int(self.frame_height-horizontal_placement*self.frame_height))
+            self.frame_dict[frame_name] = cv2.putText(
+                self.frame_dict[frame_name], frame_name, org2, self.font, self.fontScale, self.color, self.thickness, cv2.LINE_AA)
+
+    def update_final_frame(self, image, frame_name):
+        image = cv2.resize(image, (self.frame_width * self.number_of_small_frames,
+                           self.frame_height * self.number_of_small_frames))
+
+        self.frame_dict[frame_name] = image
+
+        vertical_placement = 0.95
+        horizontal_placement = 0.95
+        org1 = (int(self.frame_width-vertical_placement*self.frame_width),
+                int(self.capture_height-horizontal_placement * self.capture_height))
+        self.frame_dict[frame_name] = cv2.putText(
+            self.frame_dict[frame_name], frame_name, org1, self.font, self.fontScale, self.color, self.thickness, cv2.LINE_AA)
+
+        small_frames = []
+        for small_frame_name in list(self.frame_dict.keys())[:-1]:
+            if small_frame_name != None:
+                small_frames.append(self.frame_dict[small_frame_name])
+
+        bottom_frames = np.concatenate(small_frames, axis=1)
+
+        output = np.concatenate(
+            (self.frame_dict[frame_name], bottom_frames), axis=0)
+        if self.save:
+            self.result.write(output)
+
+        output = cv2.resize(
+            output, (int(output.shape[1]*0.6), int(output.shape[0]*0.6)))
+        cv2.imshow(frame_name + " Pipeline", output)
+
+    def __del__(self):
+        print("Closing output feed...")
+
 class TurnPrediciton():
     def __init__(self, image, scale):
         self.image = image
@@ -37,30 +144,41 @@ class TurnPrediciton():
             self.image = cv2.GaussianBlur(self.image, (7, 7), 0)
         image_scaled = self.image
 
-        return self.image, image_scaled
+        return self.image
     
-    def crop_lane(self):
-        # defining a region of interest
-        # TODO: This should be parameterized to the image size
-        region = np.array(
-            [(10, self.rows), (370, self.rows), (240, 135), (150, 135)])
+    def crop_lane(self,gray):
+        # defining region of interest so that maximum length of the lane can be detected
+        # TODO: come up with better way to crop using image shape parameters
+        region = np.array([(230, self.rows - 60), (1120, self.rows - 60), (730, 440), (610, 440)])
 
-        mask = np.zeros_like(self.image)
+        # cropping only the region of interest for lane detection
+        mask = np.zeros_like(gray)
         cv2.fillPoly(mask, pts=[region], color=(255, 255))
-        self.lane_image = cv2.bitwise_and(self.image, mask)
-        return self.lane_image
+        self.lane_image = cv2.bitwise_and(gray, mask)
+        return self.lane_image, region
 
-    def warp_lane(self):
-        pass
+    def warp_lane(self, cropped, region):
+        # warping the cropped lane into a flat plane using homography
+        # TODO: flat plane size should be proportional to the lane cropped 
+        flat_plane = np.array([(0, 0), (200, 0), (200, 500), (0, 500)], dtype=float)
+        h, status = cv2.findHomography(region, flat_plane)
+        h_inv, status_inv = cv2.findHomography(flat_plane,region)
+        warped = cv2.warpPerspective(cropped, h, (200,500))
+        warped = cv2.flip(warped,0)
+        dst = cv2.cvtColor(warped, cv2.COLOR_GRAY2BGR)
+        return warped, dst, h_inv
 
-    def detect_edges(self):
-        pass
+    def detect_edges(self, warped):
+        # detecting edges on the warped lane using canny edge detection
+        edge = cv2.Canny(warped, 50, 450)  # 50,350
+        return edge
 
-    def hough_lines(self):
-        lines = cv2.HoughLinesP(self.lane_image, 2, np.pi/180, 40, np.array(
-            []), minLineLength=self.minLineLength, maxLineGap=self.maxLineGap)
-        return lines
-    
+    def hough_lines(self,edge):
+        # finding lines on the detected edges using hough transform. parameters have been tuned such that atleast 4 lines will be generated on both the left and the right curves of the lane
+        linesP = cv2.HoughLinesP(edge, 2, np.pi / 180, 25, np.array([]), minLineLength=10, maxLineGap=150) # image, rho, theta, threshold = min # of intersections for line, lines, min, max
+        return linesP
+
+    # TODO: only find out which one is the left line and which one is the rigth line
     def line_classifier(self, lines, image):
         left = []
         right = []
@@ -111,59 +229,14 @@ class TurnPrediciton():
                          (255, 0, 0), 2, cv2.LINE_AA)
         return cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-    def lines_to_points(self):
-        pass
-
-    def fit_poly(self):
-        pass
-
-    
-def main():
-    # TODO: write turn prediction class with all the preprocessing 
-    frame = cv2.VideoCapture('/home/ishan/Documents/UMD/portfolio/projects/turn_prediction/data/challenge.mp4')
-
-    while (frame.isOpened()):
-        success, image = frame.read()
-        if not success:
-            print("\nEnd of frames\n")
-            break
-        
-        scale = 1
-        currentFrame = TurnPrediciton(image, scale)
-        rows, cols, channels = image.shape
-
-        gray_image = currentFrame.pre_processing(resize=False, gray=True, blur=False)
-
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        # defining region of interest so that maximum length of the lane can be detected
-        # TODO: come up with better way to crop using image shape parameters
-        region = np.array([(230, rows - 60), (1120, rows - 60), (730, 440), (610, 440)])
-
-        # cropping only the region of interest for lane detection
-        mask = np.zeros_like(gray)
-        cv2.fillPoly(mask, pts=[region], color=(255, 255))
-        cropped = cv2.bitwise_and(gray, mask)
-
-        # warping the cropped lane into a flat plane using homography
-        # TODO: flat plane size should be proportional to the lane cropped 
-        flat_plane = np.array([(0, 0), (200, 0), (200, 500), (0, 500)], dtype=float)
-        h, status = cv2.findHomography(region, flat_plane)
-        h_inv, status_inv = cv2.findHomography(flat_plane,region)
-        warped = cv2.warpPerspective(cropped, h, (200,500))
-        warped = cv2.flip(warped,0)
-        dst = cv2.cvtColor(warped, cv2.COLOR_GRAY2BGR)
-
-        # detecting edges on the warped lane using canny edge detection
-        edge = cv2.Canny(warped, 50, 450)  # 50,350
-
-        # finding lines on the detected edges using hough transform. parameters have been tuned such that atleast 4 lines will be generated on both the left and the right curves of the lane
-        linesP = cv2.HoughLinesP(edge, 2, np.pi / 180, 25, np.array([]), minLineLength=10, maxLineGap=150) # image, rho, theta, threshold = min # of intersections for line, lines, min, max
-
+    def lines_to_points(self,linesP, points):
         # segregating left lines and right lines based on their location in the warped image
         # TODO: coloring left lane = red and right lane = green 
         left = []
         right = []
+
+        points = cv2.cvtColor(points, cv2.COLOR_GRAY2BGR)
+
         if linesP is not None:
             for i in range(0, len(linesP)):
                 l = linesP[i][0]
@@ -180,28 +253,20 @@ def main():
             all_points_left.append((i[0],i[1]))
             all_points_left.append((i[2], i[3]))
 
-        # for i in all_points_left:
-        #     cv2.circle(dst, i, 5, (255,0,0), 1)
+        for i in all_points_left:
+            cv2.circle(points, i, 5, (0,255,255), 1)
 
         all_points_right = []
         for i in right:
             all_points_right.append((i[0],i[1]))
             all_points_right.append((i[2], i[3]))
 
-        # for i in all_points_right:
-        #     cv2.circle(dst, i, 5, (255,0,0), 1)
+        for i in all_points_right:
+            cv2.circle(points, i, 5, (0,255,255), 1)
 
-        # # code snippet to test points for detection window to check whether to continue with lane detection or use past values
-        # p1 = (0,50)
-        # p2 = (0,200)
-        # p3 = (50,200)
-        # p4 = (50,50)
-        #
-        # dst = cv2.line(dst, p1, p2, (255,0,50), 3)
-        # dst = cv2.line(dst, p2, p3, (255, 0, 50), 3)
-        # dst = cv2.line(dst, p3, p4, (255, 0, 50), 3)
-        # dst = cv2.line(dst, p4, p1, (255, 0, 50), 3)
+        return all_points_left, all_points_right, points
 
+    def fit_poly(self, all_points_left, all_points_right,dst,h_inv, image, left_fit_coefs, right_fit_coefs, x_r):
         # checking if detection can be satisfactorily performed
         # TODO: satisfactory condition should be to check for shadows or unreliable points detection/ breaks in point detection
         count = 0
@@ -228,10 +293,6 @@ def main():
 
             left_fit_coefs = np.polyfit(y_l, x_l, 2)
             right_fit_coefs = np.polyfit(y_r, x_r, 2)
-
-            # print("New Frame")
-            # print("Left curve coefficients:" + str(left_fit_coefs))
-            # print("Right curve coefficients:" + str(right_fit_coefs) + "\n")
 
             y_values = np.linspace(0, dst.shape[0] - 1, dst.shape[0])
             x_values_left = left_fit_coefs[0] * y_values ** 2 + left_fit_coefs[1] * y_values + left_fit_coefs[2]
@@ -293,13 +354,43 @@ def main():
                             fontScale, color, thickness, cv2.LINE_AA)
         final = cv2.putText(final, info2 , (100,80), font,
                             fontScale, color, thickness, cv2.LINE_AA)
+        
+        return final, left_fit_coefs, right_fit_coefs, x_r
+    
+def main():
+    # TODO: write turn prediction class with all the preprocessing 
+    frame = cv2.VideoCapture('/home/ishan/Documents/UMD/portfolio/projects/turn_prediction/data/challenge.mp4')
+    left_fit_coefs = None
+    right_fit_coefs = None
+    x_r = None
+    while (frame.isOpened()):
+        success, image = frame.read()
+        if not success:
+            print("\nEnd of frames\n")
+            break
+        
+        scale = 1
+        currentFrame = TurnPrediciton(image, scale)
+        rows, cols, channels = image.shape
 
-        # displaying the final detection result
-        # you can uncomment one of them and comment all others to properly visualize the various stages
-        cv2.imshow("lane", final)
-        # cv2.imshow("lane",dst)
-        # cv2.imshow("preview", edge)
-        # cv2.imshow("original", image)
+        gray = currentFrame.pre_processing(resize=False, gray=True, blur=False)
+        cropped, region = currentFrame.crop_lane(gray)
+
+        warped, dst, h_inv = currentFrame.warp_lane(cropped,region)
+        edge = currentFrame.detect_edges(warped)
+
+        # cv2.imshow("edge", edge)
+
+        linesP = currentFrame.hough_lines(edge)
+
+        all_points_left, all_points_right, points = currentFrame.lines_to_points(linesP, warped)
+        
+        cv2.imshow("points", points)
+
+        final, left_fit_coefs, right_fit_coefs, x_r = currentFrame.fit_poly(all_points_left, all_points_right, dst, h_inv, image, left_fit_coefs, right_fit_coefs, x_r)
+
+        cv2.imshow("polynomial", dst)
+        # cv2.imshow("lane", final)
 
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
